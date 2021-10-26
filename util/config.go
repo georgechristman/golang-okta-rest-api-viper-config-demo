fa3589171334aa2c5da8bf14588358e78cea325d
gmc07 <george@zigster.com>
2021-10-26 16:58:47 -0400

    Initial commit

---
+++ util/config.go
@@ -0,0 +1,29 @@
+package util
+
+import "github.com/spf13/viper"
+
+// Config stores all configuration of the application.
+// The values are read by viper from a config file or environment variables
+type Config struct {
+	OktaClientId string `mapstructure:"OKTA_CLIENT_ID"`
+	OktaIssuer   string `mapstructure:"OKTA_ISSUER"`
+	Audience     string `mapstructure:"OKTA_AUDIENCE"`
+}
+
+// LoadConfig reads configuration from file or environment variables
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigFile("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
